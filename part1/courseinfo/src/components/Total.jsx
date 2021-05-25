d75bee68ec0c38263abddbd2833f3609b4d4cbba
root <root@vs.dotme.group>
2021-05-25 03:52:06 +0000

    exercises 1.1 and 1.2

---
+++ part1/courseinfo/src/components/Total.jsx
@@ -0,0 +1,9 @@
+const Total = ({ parts: [part1, part2, part3] }) => {
+  return (
+    <p>
+      Number of exercises {part1.exercises + part2.exercises + part3.exercises}
+    </p>
+  );
+};
+
+export default Total;
